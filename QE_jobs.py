98de2816afbce9ec424d2b9d0d413cf3f0941b85
buldin <artbuld@gmail.com>
2024-03-20 14:49:38 +0100

    new wano

---
+++ QE_jobs.py
@@ -0,0 +1,154 @@
+import os
+import subprocess
+import yaml
+
+from ase.build import bulk
+from ase.io import read, write
+
+
+def load_pseudopotentials_dict(pseudopotentials_path):
+    """
+    Loads the dictionary of pseudopotentials from a specified path.
+
+    Parameters:
+    pseudopotentials_path (str): The path to the directory containing the pseudopotentials.yml file.
+
+    Returns:
+    dict: A dictionary where keys are element symbols and values are corresponding pseudopotential filenames.
+    """
+    os.environ['ESPRESSO_PSEUDO'] = pseudopotentials_path
+
+    with open(os.path.join(pseudopotentials_path, "pseudopotentials.yml"), "r") as file:
+        pseudopotentials_dict = yaml.safe_load(file)
+    return pseudopotentials_dict
+
+
+def run_quantum_espresso(input_file, output_file, pseudopotentials_path):
+    """
+    Executes a Quantum ESPRESSO calculation using the specified input and output files.
+
+    Parameters:
+    input_file (str): The name of the Quantum ESPRESSO input file (e.g., 'output.pwi').
+    output_file (str): The name of the Quantum ESPRESSO output file (e.g., 'output.pwo').
+    pseudopotentials_path (str): The path to the directory containing pseudopotentials.
+    """
+    os.environ['ESPRESSO_PSEUDO'] = pseudopotentials_path
+    command = ["mpirun", "-np", "1", "pw.x", "-in", input_file]
+    with open(output_file, 'w') as file:
+        subprocess.run(command, stdout=file, stderr=subprocess.STDOUT)
+
+
+def create_relax_pwi(element, lattice_param, cubic, crystal, kpts, output_filename,
+                     pseudopotentials_path, pseudopotentials_dict):
+    """
+    Creates a Quantum ESPRESSO input file ('output.pwi') for a crystal structure relaxation.
+
+    Parameters:
+    element (str): The chemical symbol of the element to create the bulk structure.
+    lattice_param (float): The lattice parameter for the bulk structure.
+    cubic (bool): Flag to determine if the structure should be cubic.
+    crystal (str): The crystal type, used by ASE.
+    kpts (tuple): A tuple representing the k-points grid (e.g., (3, 3, 3)).
+    pseudopotentials_path (str): The path to the directory containing pseudopotentials.
+    pseudopotentials_dict (dict): Dictionary of element-symbol to pseudopotential filename mappings.
+    """
+    os.environ['ESPRESSO_PSEUDO'] = pseudopotentials_path
+    pseudopotential_file = pseudopotentials_dict[element]
+    pseudopotentials = {element: pseudopotential_file}
+    structure = bulk(element, a=lattice_param, cubic=cubic)
+    input_data_relax = {
+        'calculation': 'vc-relax',
+        'cell_dofree': 'ibrav',
+    }
+
+    write(
+        output_filename,
+        structure,
+        Crystal=crystal,
+        kpts=kpts,
+        input_data=input_data_relax,
+        pseudopotentials=pseudopotentials,
+        tstress=True,
+        tprnfor=True
+    )
+
+
+def write_input_file(element, file_pattern, output_filename, crystal, kpts, pseudopotentials_path,
+                     pseudopotentials_dict):
+    """
+    Writes the input file for strain calculation or other type of calculation.
+
+    Parameters:
+    element (str): The chemical symbol of the structure's element.
+    file_pattern (str): File pattern for reading the strained structure files (not used in this template).
+    output_filename (str): Path to the output file for PWscf input strain ('output.pwi').
+    crystal (str): The crystal type, used by ASE.
+    kpts (tuple): A tuple representing the k-points grid.
+    pseudopotentials_path (str): The path to the directory containing pseudopotentials.
+    pseudopotentials_dict (dict): Dictionary of element-symbol to pseudopotential filename mappings.
+    """
+    os.environ['ESPRESSO_PSEUDO'] = pseudopotentials_path
+    pseudopotential_file = pseudopotentials_dict[element]
+    pseudopotentials = {element: pseudopotential_file}
+    input_data_static = {'calculation': 'scf'}
+    structure_strain = read(file_pattern)
+
+    write(
+        output_filename,
+        structure_strain,
+        Crystal=crystal,
+        kpts=kpts,
+        input_data=input_data_static,
+        pseudopotentials=pseudopotentials,
+        tstress=True,
+        tprnfor=True
+    )
+
+
+if __name__ == '__main__':
+    with open("rendered_wano.yml", "r") as file:
+        params = yaml.safe_load(file)
+
+    structure_file = params.get('Structure file', None)
+    if structure_file:
+        with open(structure_file, "r") as file:
+            structure_params = yaml.safe_load(file)
+    else:
+        raise ValueError("Structure file not specified or not found.")
+
+    pseudopotentials_path = '/home/ws/aj3373/pseudopotentials'
+
+    if params.get('Your path to the pseudopotential files', False) and 'pseudopotentials_path' in params:
+        pseudopotentials_path = params['custom_pseudopotentials_path']
+    pseudopotentials_dict = load_pseudopotentials_dict(pseudopotentials_path)
+
+    calculation_mode = params.get('Type of job')
+
+    if calculation_mode == 'Relaxation':
+        create_relax_pwi(
+            element=structure_params['element'],
+            lattice_param=float(structure_params['lattice_param']),
+            cubic=structure_params['cubic'],
+            crystal=structure_params['crystal'],
+            output_filename='output.pwi',
+            kpts=tuple(map(int, structure_params['kpts'].split(','))),
+            pseudopotentials_path=pseudopotentials_path,
+            pseudopotentials_dict=pseudopotentials_dict
+        )
+        input_file = 'output.pwi'
+    elif calculation_mode == 'Calculation energy':
+        write_input_file(
+            element=structure_params['element'],
+            crystal=structure_params['crystal'],
+            kpts=tuple(map(int, structure_params['kpts'].split(','))),
+            file_pattern='structure_strain.traj',
+            output_filename='output.pwi',
+            pseudopotentials_path=pseudopotentials_path,
+            pseudopotentials_dict=pseudopotentials_dict
+        )
+        input_file = 'output.pwi'
+    else:
+        raise ValueError("Unsupported mode specified in 'rendered_wano.yml'.")
+
+    output_file = "output.pwo"
+    run_quantum_espresso(input_file, output_file, pseudopotentials_path)
---
+++ README.md
@@ -0,0 +1,38 @@
+# QE_jobs
+
+**Version:** 0.0.1
+
+**Author:** J. Schaarschmidt
+
+## Description
+
+This script is utilized to perform Quantum ESPRESSO (QE) calculations for two main types of jobs: relaxation of crystal structures and energy calculation.
+
+## Inputs
+
+- `rendered_wano.yml`: A YAML configuration file specifying the type of job (`Relaxation` or `Calculation energy`), parameters for the simulation, and the path to the structure file.
+
+- `structure.yml`: The essential input file for detailing the crystal structure. It needs to specify the type of elements, lattice parameters, the crystal type, and other relevant data for the calculation.
+
+Depending on the type of job selected, different output files and processes are initiated:
+
+## Outputs
+
+For **Relaxation**:
+
+- `output.pwi`: The Quantum ESPRESSO input file created for the relaxation process, encompassing computational parameters, structural details, and instructions for the relaxation calculation.
+- `output.pwo`: The resulting file from Quantum ESPRESSO after the relaxation job, containing details about the final structure, energy, forces, and other computed properties.
+
+For **Energy Calculation**:
+
+- `structure_strain.traj`: (Optional) A file pattern to read strained structures if needed for the computation.
+- `output.pwi`: Similar to the relaxation process, this serves as the input file for Quantum ESPRESSO but tailored for energy calculation.
+- `output.pwo`: The resulting file containing the outcome of the energy calculation, including total energy, electron density, and other pertinent results.
+
+## Dependencies
+
+The successful operation of this workflow necessitates the following components:
+
+- **ASE (Atomic Simulation Environment):** A Python toolset for manipulating atoms, executing simulations, and analyzing outcomes. It serves to manage crystal structures and mold Quantum ESPRESSO input files.
+- **pwtools:** Part of the Quantum ESPRESSO package, helpful for the input and output processing of simulations.
+- **Quantum Espresso:** The core software executing the computations for material modeling based on density-functional theory.
