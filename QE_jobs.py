9a4e715784cb941bcade3272249ffd28ae6c1c73
buldin <artbuld@gmail.com>
2025-06-04 14:42:09 +0200

    fix the route to the file

--- QE_jobs.py
+++ QE_jobs.py
@@ -140,10 +140,13 @@ if __name__ == '__main__':
     cubic = True if len(set(atoms.cell.lengths())) == 1 else False
     element = atoms.get_chemical_symbols()[0]
 
-    if params.get("Your path to the pseudopotential files") is True:
-        pseudopotentials_path = os.path.expandvars(params["custom_pseudopotentials_path"])
+    if params.get('Your path to the pseudopotential files') is True:
+        pseudopotentials_path = params.get('custom_pseudopotentials_path', '').strip()
     else:
         pseudopotentials_path = '/home/ws/aj3373/pseudopotentials'
+
+    if not os.path.isfile(os.path.join(pseudopotentials_path, "pseudopotentials.yml")):
+        raise FileNotFoundError(f"Cannot find pseudopotentials.yml in path: {pseudopotentials_path}")
 
     pseudopotentials_dict = load_pseudopotentials_dict(pseudopotentials_path)
 
